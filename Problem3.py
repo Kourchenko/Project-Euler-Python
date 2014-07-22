5679da83ff913fd923f0611176ac1a0056fc3da6
TurnsOfHi <dkourchenko@yahoo.com>
2014-07-21 22:21:35 -0400

    Create Problem3.py

---
+++ Problem3.py
@@ -0,0 +1,18 @@
+def test_prime(num):    # Scales logarithmically
+    i = 2
+    while i < num:
+        if num % i == 0:
+            return False
+        i += 1
+    return True
+
+
+def prime_fact(num):  # Just needs to check if number is divisible by prime, scales linear
+    i = 2
+    while i <= num:
+        if test_prime(i) and num % i == 0:
+            print i
+        i += 1
+
+
+# Still needs work; while calculating input, takes while to finish, even after it has found the largest prime factor.
