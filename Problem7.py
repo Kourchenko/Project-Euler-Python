be225046b528f729c182a8f1b9fb33ca292b35cc
TurnsOfHi <dkourchenko@yahoo.com>
2015-02-08 23:17:19 -0800

    Create Problem7.py

---
+++ Problem7.py
@@ -0,0 +1,35 @@
+def test_prime(number):
+    base = 2
+
+    if number == 2:
+        return 2
+
+    while base < number**0.5+1:
+        if number % base == 0:
+            return False
+        base += 1
+    return True
+
+
+def to_prime(num):
+
+    start = 0
+    position = 1
+
+    prime = 2
+
+    while start <= num:
+        if test_prime(position):
+            prime = position
+            start += 1
+        position += 1
+
+    return prime
+    
+    
+def test_cases():
+  assert to_prime(10001) == 104743
+  assert to_prime(10) == 29
+  assert to_prime(1) == 1
+  assert to_prime(0) == 1
+  
