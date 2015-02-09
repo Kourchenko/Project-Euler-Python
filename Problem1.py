3bfabe053781b1058d9b4342a011e29ef014e7d0
TurnsOfHi <dkourchenko@yahoo.com>
2015-02-08 23:20:45 -0800

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -4,7 +4,7 @@ Given number, find multiples of 3 or 5 and find their sum.
 
 def multiple(number):
     total = 0
-    for i in range(number):
+    for i in range(1, number):
         if i % 3 == 0 or i % 5 == 0:
             total += i
     return total
